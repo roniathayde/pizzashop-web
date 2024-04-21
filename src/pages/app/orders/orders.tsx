7e12960576b1da30efd6e2f19a02beb0f18c5bd9
roniathayde <pontes014@gmail.com>
2024-04-21 15:45:28 -0300

    style: skeleton features on loading data

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -13,6 +13,7 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
+import { OrderTableSkeleton } from './order-table.skeleton'
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -70,6 +71,8 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
+
                 {result &&
                   result.orders.map((order) => {
                     return <OrderTableRow key={order.orderId} order={order} />
