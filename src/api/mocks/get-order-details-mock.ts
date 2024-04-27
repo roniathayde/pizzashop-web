98c06d3a66739ad94b792362b3e247d2eb35e23e
roniathayde <pontes014@gmail.com>
2024-04-26 22:58:38 -0300

    test: finish mocks

---
+++ src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,38 @@
+import { http, HttpResponse } from 'msw'
+
+import {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/orders/:orderid', ({ params }) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    customer: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '1231231231',
+    },
+    status: 'pending',
+    createdAt: new Date().toISOString(),
+    totalInCents: 5000,
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 1000,
+        product: { name: 'Pizza Pepperoni' },
+        quantity: 1,
+      },
+      {
+        id: 'order-item-2',
+        priceInCents: 2000,
+        product: { name: 'Pizza Marguerita' },
+        quantity: 2,
+      },
+    ],
+  })
+})
